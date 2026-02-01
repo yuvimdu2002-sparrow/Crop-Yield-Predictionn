f5b3cd3f882d5ca8d9e13c0a4afd864664c31a21
yuvimdu2002-sparrow <yuvimdu2002@gmail.com>
2026-02-01 19:47:49 +0530

    Update streamlit_app.py

--- source file/streamlit_app.py
+++ source file/streamlit_app.py
@@ -36,7 +36,7 @@ def add_bg_from_local(image_file):
         unsafe_allow_html=True
     )
 
-add_bg_from_local("background.jpg")  # correct path here
+add_bg_from_local("data/background.jpg")  # correct path here
 import pandas as pd
 
 import streamlit as st
